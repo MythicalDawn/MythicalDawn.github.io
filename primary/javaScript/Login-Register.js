5f8d160d26b1d13e29e7836c73df39b99cdb4d7f
NingyMD <ningy.hm@gmail.com>
2024-07-13 11:14:17 -0700

    new file:   404.html 	modified:   primary/home.html 	modified:   primary/javaScript/Login-Register.js 	new file:   primary/php/store_index.php 	modified:   store/index.html

--- primary/javaScript/Login-Register.js
+++ primary/javaScript/Login-Register.js
@@ -4,18 +4,18 @@ const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('#btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
-registerLink.addEventListener('click', ()=> {
+registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click', ()=> {
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=> {
+btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener('click', ()=> {
+iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 });
